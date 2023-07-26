03a404fa95c3d57f90b3a96510011371c2be9277
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-26 12:03:59 -0400

    Install rails-react app

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # get 'root/index'
+  get '/api/greeting', to: 'greetings#index'
+  get '/greeting', to: 'greeting#index'
+  root 'root#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
